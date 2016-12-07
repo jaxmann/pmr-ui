08c00e8767711b4cc00b32f01343ed31a4d1059b
Jonathan Axmann <jonathan.axmann09@gmail.com>
2016-12-07 11:46:04 -0500

    fix main class

--- src/main.java
+++ src/main.java
@@ -15,13 +15,7 @@ public class main {
 
 	public static final boolean DEBUG = true;
 	public static String currentUser = null;
-	public static ArrayList<User> users = new ArrayList<User>();
 	public static ArrayList<Player> players = new ArrayList<Player>();
-	public static ArrayList<String> gcdTeams = new ArrayList<String>();
-	public static ArrayList<String> gcdPlayers = new ArrayList<String>();
-	public static ArrayList<String> fullTeams = new ArrayList<String>();
-	public static ArrayList<String> fullPlayers = new ArrayList<String>();
-	public static ArrayList<HighlightLink> highlightLinks = new ArrayList<HighlightLink>();
 
 	public static void main(String[] args) {
 		//debug flag for print statements
@@ -35,8 +29,8 @@ public class main {
 		//perform search/crawl on websites, save url, verify its a smallish video file
 
 		//send a link (create an alert at first)
-		
-		
+
+
 		//////////////////////////////////////////////////////////////////
 		//after picking teams, etc
 		//do big db call to populate teams/players arraylist
@@ -45,8 +39,8 @@ public class main {
 		//possible to approximate file size before download
 		//HttpURLConnection content = (HttpURLConnection) new URL("https://docs.oracle.com/javase/7/docs/api/java/lang/Object.html").openConnection();
 		//		System.out.println(content.getContentLength());
-		
-		
+
+
 		players.add(new Player("Marco Reus", "Borussia Dortmund", "Bundesliga"));
 		players.add(new Player("Christiano Ronaldo", "Real Madrid", "La Liga"));
 		players.add(new Player("Emre Mor", "Borussia Dortmund", "Bundesliga"));
@@ -57,7 +51,7 @@ public class main {
 		players.add(new Player("Antoine Griezmann", "Atletico Madrid", "La Liga"));
 		players.add(new Player("Paul Pogba", "Manchester United", "EPL"));
 
-		
+
 		Login.main(args);
 
 		//determine if play is worth examining -- does it contain "goal"?
@@ -68,137 +62,7 @@ public class main {
 		///////////////////////////////////////////////////////////////////
 
 
-		
-		//-----------------FAKE DATA ------------------------------
-		String playAlert = "Goal by Reus! Marcelo crosses from left side and Ronaldo heads it home past Ter Stegen!";
-		//users
-		ArrayList<String> tags1 = new ArrayList<String>();
-		tags1.add("dortmund");
-		tags1.add("reus");
-		ArrayList<String> tags2 = new ArrayList<String>();
-		tags2.add("real");
-		tags2.add("ronaldo");
-		users.add(new User("jonathan.axmann09@gmail.com", 1314940, tags1));
-		users.add(new User("k.chesser@tcs.com", 1314943, tags2));
-		//gcdTeams
-		gcdTeams.add("dortmund");
-		gcdTeams.add("real");
-		//gcdPlayers
-		gcdPlayers.add("reus");
-		gcdPlayers.add("ronaldo");
-		//fullTeams
-		fullTeams.add("Borussia Dortmund");
-		fullTeams.add("Real Madrid");
-		//fullPlayers
-		fullPlayers.add("Marco Reus");
-		fullPlayers.add("Christiano Ronaldo");
 
-
-		if (isPlayWorthKeeping(playAlert)) {
-			parsePlay(playAlert);
-		}
-
-	}
-
-
-	public static boolean isPlayWorthKeeping(String playAlert) {
-		if (playAlert.toLowerCase().contains("goal")) {
-			if (main.DEBUG) {
-				System.out.println("isPlayWorthKeeping: TRUE");
-			}
-			return true;
-
-		} 
-		if (main.DEBUG) {
-			System.out.println("isPlayWorthKeeping: FALSE");
-		}
-		return false;
-	}
-
-
-	public static void parsePlay(String play) {
-		Date date = new Date(); 
-
-		HashSet<String> shortKeywords = new HashSet<String>();
-		HashSet<String> longKeywords = new HashSet<String>();
-		int[] score = null;
-
-		String[] playArr = play.split(" ");
-
-		if (main.DEBUG) {
-			System.out.println("Play as array is: " + Arrays.toString(playArr));
-		}
-
-		for (int i=0; i<playArr.length; i++) {
-			//teams
-			for (int j=0; j<gcdTeams.size(); j++) {
-				if (playArr[i].toLowerCase().contains(gcdTeams.get(j))) {
-					shortKeywords.add(gcdTeams.get(j));
-					longKeywords.add(fullTeams.get(j));
-				}
-			}
-			//players
-			for (int k=0; k<gcdPlayers.size(); k++) {
-				if (playArr[i].toLowerCase().contains(gcdPlayers.get(k))) {
-					shortKeywords.add(gcdPlayers.get(k));
-					longKeywords.add(fullPlayers.get(k));
-				}
-			}
-		}
-
-		if (main.DEBUG) {
-			System.out.println("Score should be 2 ints: " + Arrays.toString(score));
-		}
-
-
-		if (main.DEBUG) {
-			if (shortKeywords.size() == 0) {
-				System.out.println("Keywords are empty :(");
-			} else {
-				for (String q : longKeywords) {
-					System.out.println("Keyword: " + q);
-				}
-			}
-
-		}
-
-
-		Play p = new Play(date, shortKeywords, longKeywords, date, score);
-		//for each play p, start a thread
-		//add a new worker thread (newWorkerThread)
-		newWorkerThread(p);
-
-	}
-
-	public static int[] regexBuildScore(String text, String regex) {
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(text);
-		String matchingString = text.substring(matcher.start(), matcher.end());
-		Scanner s = new Scanner(matchingString);
-		int[] scoreArr = {s.nextInt(), s.nextInt()};
-		return scoreArr;
-
-	}
-
-	public static void newWorkerThread(Play p) {
-		//create one at a time
-		CrawlerThread t = new CrawlerThread(p);
-		t.run();
-	}
-
-	public String pickTeam(String team) {
-		//connect to a db and validate their input (checking permutations of what they said, e.g. "dortmund" = "Borussia Dortmund") - like watson..
-		//for now...
-		String teamPicked = "Borussia Dortmund";
-		return teamPicked;
-		//this will be different from "add team", etc, which will also check if the team already exists, etc
-	}
-
-	public String pickPlayer(String player, String team) {
-		//prompt for team first, then prompt for player
-		//validate their player against the players full/last name in db, for each player associated with every team :/
-		String playerPicked = "Marco Reus";
-		return playerPicked;
 	}
 
 }
